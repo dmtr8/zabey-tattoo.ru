e1153196d371f77bcb3a515c2b654b69e8fe0ae7
verslibremd <68556094+verslibremd@users.noreply.github.com>
2021-09-05 12:22:13 +0300

    index page

---
+++ src/scripts/app.js
@@ -0,0 +1,66 @@
+
+//document.body.style.backgroundImage = "url('../images/layout/background.jpg')";
+
+//import '../images/layout/background.jpg';
+
+import anime from 'animejs/lib/anime.es.js';
+
+require('animejs');
+import Vue from 'vue';
+require('bootstrap');
+require('jquery');
+
+
+/*
+
+/!** Inspired by coursetro.com **!/
+
+// Refs
+const wrapCta  = document.querySelector('#wrap-cta'),
+    btnCta   = document.querySelector('#cta'),
+    content  = document.querySelector('#content'),
+    btnClose = document.querySelector('#close');
+
+
+// Anime.js Commons Values for SVG Morph
+const common = {
+    targets: '.polymorph',
+    easing: 'easeOutQuad',
+    duration: 600,
+    loop: false
+};
+
+
+// Show content
+btnCta.addEventListener('click', () => {
+    // Elements apparence
+    wrapCta.classList.remove('active');
+    content.classList.add('active');
+
+    // Morph SVG
+    anime({
+        ...common,
+        points: [
+            { value: '215,110 0,110 186,86 215,0' }
+        ],
+    });
+});
+
+
+// Hide content
+btnClose.addEventListener('click', () => {
+    // Elements apparence
+    content.classList.remove('active');
+    wrapCta.classList.add('active');
+
+    // Morph SVG
+    anime({
+        ...common,
+        points: [
+            { value: '215,110 0,110 0,0 215,0' }
+        ]
+    });
+});
+
+*/
+
